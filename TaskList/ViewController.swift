2ca822049254857c14e2975eca1b475ff6c327da
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-07-02 17:31:25 +0600

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Olzhas Zhakan on 02.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
